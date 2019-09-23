484578c25980d838af0bb019b8d42931062dd9e4
Yuri Lapitsky <lapitsky@gmail.com>
2019-09-23 17:00:49 -0500

    Update action

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const token = core.getInput('GITHUB_TOKEN');
+  const token = core.getInput('github_token');
   const pr = github.context.payload.pull_request;
+  const titleRegexStr = core.getInput('title_regex');
+  const titleRegex = new RegExp(titleRegexStr);
   console.log(`The pull request object: ${JSON.stringify(pr)}`);
   const octokit = new github.GitHub(token);
   // `who-to-greet` input defined in action metadata file
@@ -14,6 +16,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  if (!pr.title.match(titleRegex)) {
+    core.setFailed("PR must refer to a Clubhouse card, or be a [rp], [hotfix]");
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
